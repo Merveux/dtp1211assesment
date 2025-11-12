8c9acec37c12b0328f357580e08304b5e4dec50a
Merveux <mervinkwizera@gmail.com>
2025-11-12 12:17:56 +0200

    Create Question14.py

---
+++ Question14.py
@@ -0,0 +1,8 @@
+storedname="Kwizera"
+storedpassword="1234"
+name=input("Enter the name:")
+password=input("Enter the password:")
+if name==storedname and password==storedpassword:
+    print("Login Successful")
+else:
+    print("Access denied")
