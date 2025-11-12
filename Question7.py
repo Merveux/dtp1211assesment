b793e6b4345e7e700eb3b5e16bac8b363f2f24ee
Merveux <mervinkwizera@gmail.com>
2025-11-12 11:46:19 +0200

    Create Question7.py

---
+++ Question7.py
@@ -0,0 +1,8 @@
+sentence=input("Enter the sentence:")
+print("The number of characters in the sentence:",len(sentence))
+print("The sentence in the upper case:",sentence.upper())"
+word="Python""
+if word in sentence:
+    print("Word found")
+else:
+    print("Word not found")
