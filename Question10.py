44981e9d354278e5b88197ae845caed74bfb8244
Merveux <mervinkwizera@gmail.com>
2025-11-12 12:00:39 +0200

    Create Question10.py

---
+++ Question10.py
@@ -0,0 +1,8 @@
+firstnumber=float(input("Enter the first number:"))
+secondnumber=float(input("Enter the second number:"))
+if firstnumber>secondnumber:
+    print(firstnumber)
+elif firstnumber<secondnumber:
+    print(secondnumber)
+else:
+    print("Please Enter the numbers with different values")
