52e3ac60c2dfa6787400c8950a661b814271a274
Merveux <mervinkwizera@gmail.com>
2025-11-12 11:37:14 +0200

    Create Question6.py

---
+++ Question6.py
@@ -0,0 +1,5 @@
+def celsiusfahrconvert():
+    tempc=float(input("Enter the temperature in celsius:"))
+    tempf= ((tempc * 9/5) + 32)
+    print("The tempeture is ",tempf,"in Fahrenheit")
+celsiusfahrconvert()
